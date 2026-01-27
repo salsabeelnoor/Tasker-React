b7eae61f09930afded237127642b8f14f9873d91
salsabeelnoor <salsabeelnoor@gmail.com>
2026-01-28 00:17:08 +0600

    edit and close feature done

--- src/task/AddTaskModal.jsx
+++ src/task/AddTaskModal.jsx
@@ -1,32 +1,34 @@
 import { useState } from "react";
-export default function AddTaskModal({ onSave, taskToUpdate }) {
-    const [task, setTask] = useState(taskToUpdate || {
-        id: crypto.randomUUID(),
-        title: "",
-        description: "",
-        tags: [],
-        priority: "",
-        isFavorite: false,
-    });
-    const [isAdd, setIsAdd] = useState(Object.is(taskToUpdate, null));
-    
-    const handleChange = (e) => {
-        const name = e.target.name;
-        let value = e.target.value;
-        if(name === "tags") {
-            value = value.split(",").map(tag => tag.trim());
-        }
-        setTask({
-            ...task,
-            [name]: value
-        })
+export default function AddTaskModal({ onSave, taskToUpdate, onCloseClick }) {
+  const [task, setTask] = useState(
+    taskToUpdate || {
+      id: crypto.randomUUID(),
+      title: "",
+      description: "",
+      tags: [],
+      priority: "",
+      isFavorite: false,
     }
+  );
+  const [isAdd, setIsAdd] = useState(Object.is(taskToUpdate, null));
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    let value = e.target.value;
+    if (name === "tags") {
+      value = value.split(",").map((tag) => tag.trim());
+    }
+    setTask({
+      ...task,
+      [name]: value,
+    });
+  };
   return (
     <>
       <div className="bg-black bg-opacity-70 h-full w-full z-10 absolute top-0 left-0"></div>
       <form className="absolute z-10 top-1/4 left-1/4 mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11">
         <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
-          Add New Task
+          {isAdd ? "Add New Task" : "Edit Task"}
         </h2>
         <div className="space-y-9 text-white lg:space-y-10">
           <div className="space-y-2 lg:space-y-3">
@@ -86,10 +88,18 @@ export default function AddTaskModal({ onSave, taskToUpdate }) {
         </div>
         <div className="mt-16 flex justify-center lg:mt-20">
           <button
-            type="submit" onClick={(event) => onSave(task, event)}
+            className="rounded bg-red-600 px-4 py-2
+          text-white transition-all hover:opacity-80"
+            onClick={onCloseClick}
+          >
+            Close
+          </button>
+          <button
+            type="submit"
+            onClick={(event) => onSave(task, event, isAdd)}
             className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
           >
-            Create new Task
+            Save
           </button>
         </div>
       </form>
--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,41 +1,60 @@
 import { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
-import AddTaskModal from "./AddTaskModal";
-
 
 export default function TaskBoard() {
-    const defaultTask = {
-        'id': crypto.randomUUID(),
-        'title': 'Sample Task',
-        'description': 'This is a sample task description.',
-        'tags': ['sample', 'task'],
-        'priority': 'medium',
-        'isFavorite': false, 
-    }
-    const [tasks, setTasks] = useState([defaultTask]);
-    const [showAddModal, setShowAddModal] = useState(false);
-    const [taskToUpdate, setTaskToUpdate] = useState(null);
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Sample Task",
+    description: "This is a sample task description.",
+    tags: ["sample", "task"],
+    priority: "medium",
+    isFavorite: false,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [taskToUpdate, setTaskToUpdate] = useState(null);
 
-    const handleAddTask = (tasknewTask, event) => {
-        event.preventDefault(); 
-        setTasks([...tasks, tasknewTask]);
-        setShowAddModal(false);
-        
+  const handleAddTask = (newTask, event, isAdd) => {
+    event.preventDefault();
+    if (isAdd) {
+      setTasks([...tasks, newTask]);
+    } else {
+      setTasks(
+        tasks.map((task) => {
+          if (task.id === newTask.id) {
+            return newTask;
+          }
+          return task;
+        })
+      );
     }
-    const handleEditTask = (task) => {
-        setTaskToUpdate(task);
-        setShowAddModal(true);
-    }
+
+    setShowAddModal(false);
+  };
+  const handleEditTask = (task) => {
+    setTaskToUpdate(task);
+    setShowAddModal(true);
+  };
+
+  const handleCloseClick = () => {
+    setShowAddModal(false);
+    setTaskToUpdate(null);
+  };
   return (
     <section className="mb-20" id="tasks">
-        {
-            showAddModal && <AddTaskModal onSave={handleAddTask} taskToUpdate={taskToUpdate}/>
-        }
+      {showAddModal && (
+        <AddTaskModal
+          onSave={handleAddTask}
+          onCloseClick={handleCloseClick}
+          taskToUpdate={taskToUpdate}
+        />
+      )}
       <div className="container">
         <div className="p-2 flex justify-end">
-          <SearchTask/>
+          <SearchTask />
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction onAddClick={() => setShowAddModal(true)} />
